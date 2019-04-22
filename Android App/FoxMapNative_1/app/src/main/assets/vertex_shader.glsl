df5ce7fd7cd8ef1b853cfbf79a1ec15f8671a3e5
BarniBl <veinskiyVA@mail.ru>
2019-04-23 00:50:48 +0300

    FirstEndVersion

--- Android App/FoxMapNative_1/app/src/main/assets/vertex_shader.glsl
+++
@@ -1,9 +0,0 @@
-//  Шейдер вершин треугольника
-//  по умолчанию точность вычислений: highp
-
-//  вектор 4х значений { X, Y, Z, ? }
-attribute vec4 a_Position;
-
-void main() {
-    gl_Position = a_Position;
-}

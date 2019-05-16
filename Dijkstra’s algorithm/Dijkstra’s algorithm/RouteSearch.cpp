e33b5c85ca9d47b4f7bda9e267dc4853cd070d79
BarniBl <veinskiyVA@mail.ru>
2019-05-16 11:44:57 +0300

    Удалено дублирование проекта

--- Dijkstra’s algorithm/Dijkstra’s algorithm/RouteSearch.cpp
+++ Dijkstra’s algorithm/Dijkstra’s algorithm/RouteSearch.cpp
@@ -6,7 +6,6 @@ RouteSearch<T>::RouteSearch(const struct Hall* halls, const int hallsNumber,
 	const struct Room* rooms, const int roomsNumber,
 	const struct MoveObject* moveObjects, const int moveObjectsNumber,
 	const int start, const int end, const T**const Matrix){
-
 }
 
 template<class T>

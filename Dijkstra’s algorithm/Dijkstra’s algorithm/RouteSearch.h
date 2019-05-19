2bc493d3a9f891c35bfad032f388ac37549e1fbf
BarniBl <veinskiyVA@mail.ru>
2019-05-19 20:28:21 +0300

    Обновлён поиск пути

--- Dijkstra’s algorithm/Dijkstra’s algorithm/RouteSearch.h
+++ Dijkstra’s algorithm/Dijkstra’s algorithm/RouteSearch.h
@@ -5,16 +5,15 @@
 template<class T>
 class RouteSearch{
 public:
-	RouteSearch(const std::vector<Hall> &Halls, const std::vector<Room> &Rooms,
-		const int StartId, const int EndId);
-	~RouteSearch();
+	RouteSearch(const std::vector<Hall> &Halls, const std::vector<Room> &Rooms);
+	int RefreshData(const std::vector<Hall> &Halls, const std::vector<Room> &Rooms);
+	int BuildRoute(int StartID, int EndID);
 	const std::vector<int> &GetRoute();
-	int GetRouteLen();
+	~RouteSearch();
 private:
 	bool ConvertRouteData();
 	DijkstrasAlgorithm<T> SearchMethod;
 	GraphConstructor<T> Graph;
 	std::vector<int> Route;
-	int RouteLen;
 };
 

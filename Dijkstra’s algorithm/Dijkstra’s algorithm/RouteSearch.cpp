03b09b0b9315be8851b759d5c0af0c9fd75f82a0
BarniBl <veinskiyVA@mail.ru>
2019-05-19 23:20:00 +0300

    Увеличина БД

--- Dijkstra’s algorithm/Dijkstra’s algorithm/RouteSearch.cpp
+++ Dijkstra’s algorithm/Dijkstra’s algorithm/RouteSearch.cpp
@@ -2,8 +2,7 @@
 #include "RouteSearch.h"
 
 template<class T>
-RouteSearch<T>::RouteSearch(const std::vector<Hall> &Halls, const std::vector<Room> &Rooms,
-	const int StartId, const int EndId){
+RouteSearch<T>::RouteSearch(const std::vector<Hall> &Halls, const std::vector<Room> &Rooms){
 }
 
 template<class T>

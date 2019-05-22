f3d5d424890c94c620469d8f3cfef945c3fdea32
Kuznecova Nastya <48177932+suova@users.noreply.github.com>
2019-05-22 17:18:05 +0300

    Update DBMaster.h

--- C++Server/DBMaster.h
+++ C++Server/DBMaster.h
@@ -3,31 +3,34 @@
 
 class DBMaster{
 public:
-    DBMaster(std::string folder, std::string dbname, std::string* SQLQuery);
-    DBMaster(std::string connectionString, std::string* SQLQuery);
-    DBMaster(std::string connectionString);
-    ~DBMaster();
-    int ReadAllData();
-    int ReadHalls();
-    int ReadRooms();
-    int WriteAllData(struct Hall* halls, int hallsNumber, struct Room* rooms, int roomsNumber, struct MoveObject* moveObjects, int moveObjectsNumber);
-    bool WriteHalls(struct Hall* halls, int hallsNumber);
-    bool WriteRooms(struct Room* rooms, int roomsNumber);
-    const std::vector<Hall> GetHalls();
-    const std::vector<Room> GetRooms();
-    const int GetID();
+	DBMaster(std::string folder, std::string dbname, std::vector<std::string> SQLQuery);
+	DBMaster(std::string connectionString, std::vector<std::string> SQLQuery);
+	DBMaster(std::string connectionString);
+	~DBMaster();
+	int ReadAllData();
+	int ReadHalls();
+	int ReadRooms();
+	int ReadInfo();
+	int WriteAllData(struct Hall* halls, int hallsNumber, struct Room* rooms, int roomsNumber, struct MoveObject* moveObjects, int moveObjectsNumber);
+	bool WriteHalls(struct Hall* halls, int hallsNumber);
+	bool WriteRooms(struct Room* rooms, int roomsNumber);
+	const std::vector<Hall> &GetHalls();
+	const std::vector<Room> &GetRooms();
+	const Info &GetInfo();
 private:
-    //int CallbackHall(void *data, int argc, char **argv, char **azColName);
-    //int CallbackRoom(void *data, int argc, char **argv, char **azColName);
-    //int CallbackDoor(void *data, int argc, char **argv, char **azColName);
-    //int CallbackRoomHall(void *data, int argc, char **argv, char **azColName);
-    std::vector<Hall> Halls;
-    std::vector<Room> Rooms;
-
-    std::string ConnectionString;
-    std::string ReadHallsSQLQuery;
-    std::string ReadRoomsSQLQuery;
-    std::string ReadLift;
-
-    int ID;
+	static int CallbackHall(void *data, int argc, char **argv, char **azColName);
+	static int CallbackHallHall(void *data, int argc, char **argv, char **azColName);
+	static int CallbackRoom(void *data, int argc, char **argv, char **azColName);
+	static int CallbackDoor(void *data, int argc, char **argv, char **azColName);
+	static int CallbackRoomHall(void *data, int argc, char **argv, char **azColName);
+	static int CallbackInfo(void *data, int argc, char **argv, char **azColName);
+	static int GetHallIndex(std::vector<Hall> Halls, std::string HallID);
+	static int GetRoomIndex(std::vector<Room> Rooms, std::string RoomID);
+	std::vector<Hall> Halls;
+	std::vector<Room> Rooms;
+	std::string ConnectionString;
+	std::string ReadHallsSQLQuery;
+	std::string ReadRoomsSQLQuery;
+	std::string ReadInfoSQLQuery;
+	Info Information;
 };
